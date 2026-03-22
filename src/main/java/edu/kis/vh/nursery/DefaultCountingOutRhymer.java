638fe4abf11dd72dce79c656b45b8e90e23a1940
Adrian Ubych <255722@edu.p.lodz.pl>
2026-03-22 22:38:57 +0100

    9-12 accesory i hermetyzacja

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,7 +4,11 @@ public class DefaultCountingOutRhymer {
 
     private int[] numbers = new int[12];
 
-    public int total = -1;
+    public int getTotal() {
+        return total;
+    }
+
+    private int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,17 +16,17 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return getTotal() == -1;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return getTotal() == 11;
     }
 
     protected int peekaboo() {
         if (callCheck())
             return -1;
-        return numbers[total];
+        return numbers[getTotal()];
     }
 
     public int countOut() {
